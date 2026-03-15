ae3494783b5428698e522daee1d5c85832d805f5
Bodoni <bodoni.jovan@icloud.com>
2026-03-15 11:32:57 +0100

    Trim + fix

--- 02-javascript-data-types/2-pick/index.js
+++ 02-javascript-data-types/2-pick/index.js
@@ -4,31 +4,9 @@
  * @param {...string} fields - the properties paths to pick
  * @returns {object} - returns the new object
  */
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
-const fruits = {
- apple: 2,
- orange: 4,
- banana: 3
-};
-
-=======
->>>>>>> 0239335 (fix remarks)
->>>>>>> c0a498e (fix)
 export const pick = (obj, ...fields) => {
     const res =  Object.fromEntries(
         Object.entries(obj).filter( ([key]) => fields.includes(key) )
     );
     return res;
 };
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
-
-
-console.log(pick(fruits, 'apple', 'banana')); // { apple: 2, banana: 3 }
-
-=======
->>>>>>> 0239335 (fix remarks)
->>>>>>> c0a498e (fix)
--- 03-objects-arrays-intro-to-testing/3-trim-symbols/index.js
+++ 03-objects-arrays-intro-to-testing/3-trim-symbols/index.js
@@ -5,5 +5,26 @@
  * @returns {string} - the new string without extra symbols according passed size
  */
 export function trimSymbols(string, size) {
+    if ( string === undefined || (size === 0) ) return '';
+    if ( size === undefined ) return string;
+    const res = [];
+    let count = 1,
+        i = 0,
+        len = string.length;
+  
+    while( i < len ){
+      if ( string[i] === string[i-1] ) {
+            count++;
+        } else {
+            count = 1;
+        }
+        if ( count <= size ) {
+            res.push(string[i]);
+        }
+      i++;
+    }
 
+    return res.join('');
 }
+
+trimSymbols('xxxaaxx', 1); //xax
